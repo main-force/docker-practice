43a8fae162cdbaf768536c22ea18206e0061af4e
김주력 <ryan9423@hanyang.ac.kr>
2023-07-14 09:34:48 +0900

    add test file

---
+++ test_print_hello.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pytest
+from print_hello_service import print_hello  # replace with the name of your python file
+
+
+def test_return_hello():
+    assert print_hello.return_hello() == 'hello'
